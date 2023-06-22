1c53ba6235dbf3d35a7907d3f60df5834f582ab9
João Augusto Pereira Vieira <89317386+Jopereira2005@users.noreply.github.com>
2023-06-22 13:50:00 +0000

    home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 
@@ -6,8 +7,23 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, CommonModule],
 })
 export class HomePage {
   constructor() {}
+
+  calculo: string = '';
+  resultado!: string;
+
+  adicionarCalculo(n: string) {
+
+    this.calculo += n;
+  
+    if(n == "="){
+      this.resultado = this.calculo
+      // this.resultado = eval(this.calculo)
+    }
+    
+  }
+
 }
