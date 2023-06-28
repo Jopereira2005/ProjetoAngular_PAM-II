9ca862520b6867ff40a5ddbbb327058b62d97023
João Augusto Pereira Vieira <89317386+Jopereira2005@users.noreply.github.com>
2023-06-28 04:58:43 +0000

    correções e mais ou menos

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,18 +12,73 @@ import { IonicModule } from '@ionic/angular';
 export class HomePage {
   constructor() {}
 
+  sinais = ["/", "*", "-", "+"]
   calculo: string = '';
   resultado!: string;
 
   adicionarCalculo(n: string) {
-
-    this.calculo += n;
-  
-    if(n == "="){
-      this.resultado = this.calculo
-      // this.resultado = eval(this.calculo)
-    }
-    
+    if (n === "="){
+      this.calcular()
+    }  else if (n == '.') {
+      this.calculo += this.ponto(n) 
+    } else if (this.sinais.includes(n) == true){
+      let calcArray = []
+      calcArray = this.calculo.split("").reverse()
+      if (this.sinais.includes(calcArray[1]) == true) {
+        calcArray.splice(0, 3)
+        this.calculo = calcArray.reverse().join('')
+        this.calculo += `,${n},`;
+      } else {
+        this.calculo += `,${n},`;
+      }
+    } else {
+      this.calculo += n;
+    }  
   }
 
+  calcular() {
+    let calculoF = this.calculo.split(',')
+    this.calculo = calculoF.join('')
+    this.resultado = eval(this.calculo)
+  }
+
+  c() {
+    this.calculo = '' 
+    this.resultado = ''
+  }
+
+  del() {
+    this.resultado = ''
+    let calcArray = []
+    calcArray = this.calculo.split('').reverse()
+    if (this.sinais.includes(calcArray[1]) == true) {
+      calcArray.splice(0, 3)
+    } else
+    calcArray.shift()
+    this.calculo = calcArray.reverse().join('')
+  }
+
+  mais_menos() {
+    let calcArray = []
+    calcArray = this.calculo.split(",").reverse()
+    console.log(calcArray.reverse())
+    if (calcArray[0] != ',') {
+      calcArray[0] = `(-${calcArray[0]})`
+      this.calculo = calcArray.reverse().join()
+    } else {
+      this.calculo = calcArray.reverse().join()
+    }
+
+  }
+
+  ponto(valor: string) {
+    let calcArray = []
+    calcArray = this.calculo.split("").reverse()
+    if (calcArray[0] == ',') {
+      valor = "0."
+    } else if (calcArray[0] == "." || calcArray[0] == ')' || calcArray[1] == '.') {
+      valor = ""
+    }
+    return valor
+  }
 }
