26157ca4b5dedfaf3a929540c2e2d2c4f9da6105
João Augusto Pereira Vieira <89317386+Jopereira2005@users.noreply.github.com>
2023-06-29 04:22:07 +0000

    correções

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,73 +12,141 @@ import { IonicModule } from '@ionic/angular';
 export class HomePage {
   constructor() {}
 
+  numeros = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
   sinais = ["/", "*", "-", "+"]
-  calculo: string = '';
+  calculo: string = "";
+  calculoA: string = "";
   resultado!: string;
 
   adicionarCalculo(n: string) {
-    if (n === "="){
-      this.calcular()
-    }  else if (n == '.') {
+    let calcArray = []
+    calcArray = this.calculo.split("").reverse()
+
+     if (n == ".") {
       this.calculo += this.ponto(n) 
+    } else if (n == "%") {
+      this.porcentagem()
     } else if (this.sinais.includes(n) == true){
-      let calcArray = []
-      calcArray = this.calculo.split("").reverse()
       if (this.sinais.includes(calcArray[1]) == true) {
         calcArray.splice(0, 3)
         this.calculo = calcArray.reverse().join('')
         this.calculo += `,${n},`;
+      } else if (this.calculo == ""){
       } else {
         this.calculo += `,${n},`;
-      }
+      } 
     } else {
       this.calculo += n;
-    }  
+    }
+    this.atualizarAparencia()
   }
 
   calcular() {
+    //cria uma string sem , para o eval dar certo
     let calculoF = this.calculo.split(',')
-    this.calculo = calculoF.join('')
-    this.resultado = eval(this.calculo)
+    this.resultado = eval(calculoF.join(''))
+    this.atualizarAparencia()
   }
 
   c() {
-    this.calculo = '' 
+    this.calculo = ''
     this.resultado = ''
+    this.atualizarAparencia()
   }
 
   del() {
     this.resultado = ''
     let calcArray = []
+    let calcArrayI = []
+
     calcArray = this.calculo.split('').reverse()
-    if (this.sinais.includes(calcArray[1]) == true) {
-      calcArray.splice(0, 3)
-    } else
-    calcArray.shift()
-    this.calculo = calcArray.reverse().join('')
+    calcArrayI = this.calculo.split(',').reverse()
+
+     // variavel auxilizar criada para ajudar nas verificações
+    let auxiliar = calcArrayI[0]
+    if (calcArray[0] == ")") {
+      calcArrayI.splice(0, 1, "")
+      this.calculo = calcArrayI.reverse().join()
+    } else if (this.sinais.includes(calcArrayI[1]) == true && this.numeros.includes(auxiliar[0]) == false) {
+      console.log(auxiliar)
+      console.log(calcArrayI)
+      if (auxiliar[0] == "(") {
+        calcArrayI.splice(0, 1, "")
+      } else {
+        calcArrayI.splice(0, 2)
+      }
+      this.calculo = calcArrayI.reverse().join()
+    } else {
+      console.log(calcArray)
+      calcArray.shift()
+      this.calculo = calcArray.reverse().join("")
+    }
+    this.atualizarAparencia()
+
   }
 
   mais_menos() {
+    // cria e inverte o array, para ser mais facil o uso e separa os sinais e numeros
     let calcArray = []
     calcArray = this.calculo.split(",").reverse()
-    console.log(calcArray.reverse())
-    if (calcArray[0] != ',') {
+    let auxiliar = calcArray[0]
+    console.log(auxiliar)
+    console.log(auxiliar[0])
+    if (calcArray[0] == ")") {
+    }
+    if (auxiliar[0] == "(") {
+    } else if (this.sinais.includes(calcArray[1]) == true && this.numeros.includes(auxiliar[0]) == false) {
+      console.log(calcArray)
+      this.calculo = calcArray.reverse().join()
+    } else if (this.numeros.includes(auxiliar[0]) == true) {
+      if (calcArray[1] == ".") {
+        console.log(calcArray)  
+        calcArray.splice(0, 3, `(-${calcArray[2]}.${calcArray[0]})`)
+        this.calculo = calcArray.reverse().join()
+      } else {
+        console.log(calcArray)
       calcArray[0] = `(-${calcArray[0]})`
       this.calculo = calcArray.reverse().join()
-    } else {
+      }
+    } else{
+      console.log(calcArray)
+      calcArray[0] = `(-${calcArray[0]})`
       this.calculo = calcArray.reverse().join()
     }
+    this.atualizarAparencia()
+  }
+  ponto(ponto: string) {
+    ponto = `,${ponto},`
+    // cria e inverte o array, para ser mais facil o uso e separa os sinais e numeros
+    let calcArray = []
+    calcArray = this.calculo.split(",").reverse()
+    // variavel auxilizar criada para ajudar nas verificações
+    let auxiliar = calcArray[0]
 
+    // verifica se o primeiro valor é um sinal, se for, vai ser colocado na expressão 0., ex: 1+0.
+    console.log(auxiliar)
+    if (this.sinais.includes(calcArray[1]) == true && calcArray[0] == "") {
+      ponto = "0,.,"
+    } else if (auxiliar[-1] == "%") {
+      ponto = "0,.,"
+    // verifica se há repeticção do for ., ou ) do mais ou menos, ou se a repetição do ponto e
+    } else if (calcArray[1] == "." || auxiliar[0] == "(") {
+      ponto = ""
+    }
+    this.atualizarAparencia()
+    return ponto
   }
 
-  ponto(valor: string) {
+  atualizarAparencia() {
+    this.calculoA = this.calculo.split(",").join("")
+  }
+  porcentagem () {
     let calcArray = []
-    calcArray = this.calculo.split("").reverse()
-    if (calcArray[0] == ',') {
-      valor = "0."
-    } else if (calcArray[0] == "." || calcArray[0] == ')' || calcArray[1] == '.') {
-      valor = ""
-    }
-    return valor
+    calcArray = this.calculo.split(",").reverse()
+
+    calcArray[0] = `${calcArray[0]}%`
+    this.calculo = calcArray.reverse().join()
+
   }
 }
+
